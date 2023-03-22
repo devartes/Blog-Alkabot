9e1338d96a8bcb0c19b18e885d7788a7f7ed354b
devartes <anacavalcante731@gmail.com>
2023-03-22 08:31:22 -0300

    feat: add CSS Modules to Header and update content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/app.scss";
+import "./styles/app.module.scss";
 import { Blog } from "./page/Blog";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
